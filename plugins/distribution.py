544570269efc269343a231f0d1ffc7d8ba242c54
lidor ettinger <lidor.ettinger@gmail.com>
2022-11-03 10:24:47 +0200

    support airflow 2.0

--- plugins/distribution.py
+++ plugins/distribution.py
@@ -1,11 +1,36 @@
 # This is the class you derive to create a plugin
 from airflow.plugins_manager import AirflowPlugin
-import os
-from flask import Blueprint
-from flask_admin import BaseView, expose
-import pkg_resources
 
-def make_tree(path):
+import pkg_resources
+import os, yaml, json
+from flask import Blueprint
+from flask_appbuilder import expose, BaseView as AppBuilderBaseView
+
+# Importing base classes that we need to derive
+from airflow.hooks.base import BaseHook
+from airflow.providers.amazon.aws.transfers.gcs_to_s3 import GCSToS3Operator
+
+# Will show up in Connections screen in a future version
+class PluginHook(BaseHook):
+    pass
+
+
+# Will show up under airflow.macros.test_plugin.plugin_macro
+# and in templates through {{ macros.test_plugin.plugin_macro }}
+def plugin_macro():
+    pass
+
+
+# Creating a flask blueprint to integrate the templates and static folder
+bp = Blueprint(
+    "test_plugin",
+    __name__,
+    template_folder="templates",  # registers airflow/plugins/templates as a Jinja template folder
+    static_folder="static",
+    static_url_path="/static/test_plugin",
+)
+
+def extended_make_tree(path):
     tree = dict(name=os.path.basename(path), children=[])
     try: lst = os.listdir(path)
     except OSError:
@@ -14,43 +39,64 @@ def make_tree(path):
         for name in lst:
             fn = os.path.join(path, name)
             if os.path.isdir(fn):
-                tree['children'].append(make_tree(fn))
+                tree['children'].append(extended_make_tree(fn))
             else:
-                tree['children'].append(dict(name=name))
+                with open(fn, "r") as stream:
+                    try:
+                        yaml_object = yaml.safe_load(stream)
+                        pipeline = json.dumps(yaml_object, indent=2, sort_keys=True, default=str)
+                        tree['children'].append(dict(name=name, pipeline=yaml_object))
+                    except yaml.YAMLError as exc:
+                        print(exc)
     return tree
 
-def get_python_packages():
-    dists = [str(d) for d in pkg_resources.working_set]
-    return dists
-
 # Creating a flask admin BaseView
-class distribution(BaseView):
+class Infrastructure(AppBuilderBaseView):
+    default_view = "test"
 
     @expose('/')
     def test(self):
-        dists = get_python_packages()
+        dists = [str(d) for d in pkg_resources.working_set]
         # in this example, put your test_plugin/test.html template at airflow/plugins/templates/test_plugin/test.html
-        return self.render("distribution.html", dists=dists)
-distribution = distribution(category="Infrastructure", name="Distribution List")
+        return self.render_template("distribution.html", dists=dists)
 
-bp = Blueprint(
-    "infrastructure_plugin", __name__,
-    template_folder='templates', # registers airflow/plugins/templates as a Jinja template folder
-    static_folder='static',
-    static_url_path='/static/test_plugin')
-
-class dags(BaseView):
-
+class DagsTree(AppBuilderBaseView):
+    default_view = "test"
+    
     @expose('/')
     def test(self):
-        path = os.path.expanduser(u'/opt/airflow/dags')
-        tree = make_tree(path)
+        path = os.path.expanduser(u'/opt/airflow/dags/pipelines')
+        tree = extended_make_tree(path)
         # in this example, put your test_plugin/test.html template at airflow/plugins/templates/test_plugin/test.html
-        return self.render("dirtree.html", tree=tree)
-dags = dags(category="Infrastructure", name="Tree List")
+        return self.render_template("dirtree.html", tree=tree)
+
+# Creating a flask appbuilder BaseView
+class TestAppBuilderBaseNoMenuView(AppBuilderBaseView):
+    default_view = "test"
+
+    @expose("/")
+    def test(self):
+        return self.render_template("test_plugin/test.html", content="Hello galaxy!")
+
+
+v_appbuilder_view = Infrastructure()
+v_appbuilder_package = {
+    "name": "python packages",
+    "category": "Infrastructure",
+    "view": v_appbuilder_view,
+}
+
+v_appbuilder_view_infra_dags_tree = DagsTree()
+v_appbuilder_package_infra_dags_tree = {
+    "name": "Dags Tree",
+    "category": "Infrastructure",
+    "view": v_appbuilder_view_infra_dags_tree,
+}
 
 # Defining the plugin class
 class AirflowTestPlugin(AirflowPlugin):
-    name = "infrastructure_plugin"
-    admin_views = [distribution, dags]
+    name = "test_plugin"
+    hooks = [PluginHook]
+    macros = [plugin_macro]
     flask_blueprints = [bp]
+    appbuilder_views = [v_appbuilder_package, v_appbuilder_package_infra_dags_tree]
